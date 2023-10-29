4a7aa3c20ed7c39d1150cdd204be6000e2571b5c
Jose <josemanuel21302@gmail.com>
2023-10-29 04:05:08 -0700

    finished challenge 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,27 @@
-alert("Hello World");
+// alert("Hello World");
+// console.log("Jose");
+
+/*
+//remember that the variable only holds the value and not the datatype. The value holds the datatype
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+*/
+//==================================================
+//12 let, const, and var
+
+//perfectly to "REASSIGNING" a variable with a let
+let age = 30;
+age = 31;
+
+//the value of const cannot be changed
+const birthYear = 1991;
+//birthYear = 1990; // this will cause an error
+//when using const you need to assign something to it
+// const job;//this will cause an error
+
+//var at first sight will pretty much work the same as let
+
+var job = "programmer";
+job = "teacher"; // var has no problem redeclaring
+console.log(job);
