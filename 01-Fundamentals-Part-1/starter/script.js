1502e0b01fdd7c695ad683a27103b25a65efd6c6
Jose <josemanuel21302@gmail.com>
2023-10-31 01:19:32 -0700

    practice the switch statments and added some interesting notes about them

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -43,3 +43,44 @@ if (BMIMark > BMIJohn) {
 } else {
   console.log("John's BMI is higher than Mark's!");
 }
+
+//Type conversion and coercion
+// you can convert strings into a number with type conversion
+
+const inputYear = "1991"; // we have to convert this string into a number so that it can add instead of concat when adding with a number datatype
+console.log(Number(inputYear), inputYear); // Number function will convert other datatypes into a Number
+
+//type Coercion
+let n = "1" + 1; //this will be '11' because it will concat the string ' 1 ' with number 1 converting 1 into '1' and contats
+n = n - 1; //now that we have '11' then we will have to subtract it by one and
+// since there is no subtraction in strings for any reasons then it will have to turn the string into a number
+console.log(n); // outputs 10
+
+//falsy values
+//falsy values are values that are natually false when datatype is switch into a boolean datatype
+console.log("these are falsy values");
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(""));
+
+console.log(Boolean({})); // this is true
+
+const money = 0;
+
+if (money) {
+  // in this statement where money is inputed. JS will try to convert it into a boolean. Meaning that it will turn 0 into a boolean and it's false
+  console.log("you have some money");
+} else {
+  console.log("get a job");
+}
+
+//Switch statements
+
+const day = "Monday";
+
+switch (day) {
+  case "Monday": //here does a strict comparison meaning does '==='
+    console.log("today is monday!!");
+    //the break is important because if there is no break then it will go on to the next case and run the code and stop there and hit the break there
+    break;
+}
