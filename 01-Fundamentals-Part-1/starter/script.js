aeb11851b9adc9939e46365cb88a5dd04734bb11
Jose Martinez <josemartinez@Joses-Air.lan>
2023-11-02 06:43:45 -0700

    fix syntax error

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -89,7 +89,10 @@ switch (day) {
 
 const bill = 275;
 
-// it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+/*Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant.
+In his country, it's usual to tip 15% if the bill value is between 50 and 300. 
+If the value is different, the tip is 20%.const tip = bill * (50 <= bill <= 300 ? 0.15 : 0.2); */
+
 const tip = bill * (50 <= bill <= 300 ? 0.15 : 0.2);
 
 console.log(
