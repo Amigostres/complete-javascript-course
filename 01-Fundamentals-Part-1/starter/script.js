4dc6635c0c93509366ac2d62f75eb6cc1bf1ebb2
Jose Martinez <josemartinez@Joses-Air.lan>
2023-11-02 06:42:52 -0700

    challenge 4 completed

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -84,3 +84,14 @@ switch (day) {
     //the break is important because if there is no break then it will go on to the next case and run the code and stop there and hit the break there
     break;
 }
+
+//ternary operators
+
+const bill = 275;
+
+// it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+const tip = bill * (50 <= bill <= 300 ? 0.15 : 0.2);
+
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);
